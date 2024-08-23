56f9f53b763e2f5be33b15fdefa71c13ff419858
Ayushman Singh <126311398+ayushman31@users.noreply.github.com>
2024-08-24 03:54:09 +0530

    Update index.js

--- week-3/index.js
+++ week-3/index.js
@@ -38,38 +38,24 @@ function addElement(){
 
     const parentEl = document.querySelector(".list");
 
-    // Create a new list-row
     const newRow = document.createElement('div');
     newRow.setAttribute("class", 'list-row');
 
-    // Create new list-item span
     const newItem1 = document.createElement('span');
     newItem1.setAttribute("class", 'list-item');
     newItem1.innerText = `${inputText} - ₹${inputPrice}`;
 
-    // Create remove button span
     const newItem2 = document.createElement('span');
     newItem2.setAttribute("class", 'remove');
     newItem2.setAttribute("onClick", 'deleteElement(this)');
 
-    // Create img for remove button
     const newItem3 = document.createElement('img');
     newItem3.setAttribute("src", "https://www.citypng.com/public/uploads/preview/ps-controller-cross-x-white-button-icon-png-733961695032917ke2zz3sjxb.png");
 
-    // Append img to remove span
     newItem2.appendChild(newItem3);
-
-    // Append list-item and remove span to list-row
     newRow.appendChild(newItem1);
     newRow.appendChild(newItem2);
-
-    // Append list-row to list
     parentEl.appendChild(newRow);
-
-   
-    // Clear input fields
     element1.value = '';
     element2.value = '';
-
-    
 }
